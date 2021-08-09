6c45647ea8cbcd5e4bebc794c815af1fa20c48df
askwuxue <972523419@qq.com>
2021-08-09 23:58:17 +0800

    import Button compoent ans use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Button, { ButtonType, ButtonSize } from "./components/Button/Button";
 function App() {
   return (
     <div className="App">
+      <Button disabled>Default</Button>
+      <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled>Link</Button>
+      <Button size={ButtonSize.Large}>登录</Button>
     </div>
   );
 }
