b81b3971cdb787b0b1e2b7cfd8bc34442504997f
askwuxue <972523419@qq.com>
2021-08-16 07:01:40 +0800

    use Button component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Button, { ButtonType, ButtonSize } from './components/Button/Button'
 function App () {
   return (
     <div className='App'>
-      <Button disabled>Default</Button>
-      <Button>Default</Button>
-      <Button>Default</Button>
+      <Button disabled className='aaaa'>Default</Button>
+      <Button size={ButtonSize.Small} onClick={e => e.preventDefault()}>Default</Button>
+      <Button btnType={ButtonType.Primary} autoFocus>Default</Button>
+      <Button btnType={ButtonType.Danger}>Default</Button>
+      <Button btnType={ButtonType.Link} href='https://www.baidu.com'>Link</Button>
       <Button btnType={ButtonType.Link} href='https://www.baidu.com' disabled>Link</Button>
       <Button size={ButtonSize.Large}>登录</Button>
     </div>
